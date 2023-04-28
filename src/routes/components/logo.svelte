63f04af6c4e3f1cbf5d1d86b3b6a6cd22628ed9d
Juli0q <juold@proton.me>
2023-04-28 14:18:40 +0200

    Add Logo Component
    Add a logo component to simplify inclusion of logo in new components

---
+++ src/routes/components/logo.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let toolName = ""
+    import { goto } from '$app/navigation';
+    
+    function gotoHome() {
+        goto("/")
+    }
+</script>
+
+<div class="flex place-content-center justify-center place-items-center gap-2 p-4 group cursor-pointer" on:click={gotoHome} on:keydown={gotoHome}>
+    <img src="/images/icons/toolbox.png" alt="dbd-toolbox" class="h-16 object-contain group-hover:h-20 transition-all">
+    <div>
+        <p class="text-2xl text-white customFont group-hover:text-3xl transition-all">dbd.tools</p>   
+        <p class="text-xs text-white customFont group-hover:text-sm transition-all">{toolName}</p>
+    </div>
+</div>
+
