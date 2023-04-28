a19a9483509e03a00a1eb880d361853b02c2c9bf
Juli0q <juold@proton.me>
2023-04-28 12:39:40 +0200

    Add custom error screen

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+
+<script>
+    import { page } from '$app/stores';
+
+    let gifs = [
+        "NotFound.gif",
+        "NotFound2.gif",
+        "NotFound3.gif",
+        "NotFound4.gif"
+    ]
+
+    let gif = gifs[Math.floor(Math.random()*gifs.length)]
+    let srcCode = "/images/icons/NotFound/" + gif
+</script>
+
+<div class="flex flex-col justify-center h-screen w-screen items-center place-content-center text-center">
+    <img src={srcCode} alt="NotFound" class="object-contain h-2/3 ">
+    <p class="text-white text-4xl">{$page.status}</p>
+    <p class="text-white text-4xl">{$page.error.message}</p>
+</div>
